77f07b669f571674b772ff0c5c7d69ad96517a37
Owen <oritzau2021@gmail.com>
2024-06-27 13:37:39 -0400

    cleaning up

--- simple_sequential.c
+++
@@ -1,9 +0,0 @@
-#include <math.h>
-#include "simple_sequential.h"
-
-// Simple network with 1 input layer, 1 hidden layer, and 1 output node
-
-double sigmoid(double x)
-{
-	return 1 / ( 1 + exp((-x)) );
-}
